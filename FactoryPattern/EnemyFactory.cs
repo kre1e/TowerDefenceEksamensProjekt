377a82506dac7de7cc4a801f454f7ac07f992a06
kre1e <olesendk@hotmail.com>
2021-05-31 11:17:10 +0200

    Update EnemyFactory.cs

--- FactoryPattern/EnemyFactory.cs
+++ FactoryPattern/EnemyFactory.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace TowerDefenceEksamensProjekt.FactoryPattern
 {
-    class EnemyFactory : Factory
+    internal class EnemyFactory : Factory
     {
         private static EnemyFactory instance;
 
