06caae2858d32acaa1d31e829666bfa6bb99167c
Hoffe <djkagemand1@gmail.com>
2021-05-31 16:28:44 +0200

    Fix Keie

--- GameWorld.cs
+++ GameWorld.cs
@@ -108,19 +108,19 @@ namespace TowerDefenceEksamensProjekt
         }
     }
 
-    public void DrawCollisionBox(GameObject go)
-    {
-        //Der laves en streg med tykkelsen 1 for hver side af Collision.
-        Rectangle topLine = new Rectangle(go.Collision.X, go.Collision.Y, go.Collision.Width, 1);
-        Rectangle bottomLine = new Rectangle(go.Collision.X, go.Collision.Y + go.Collision.Height, go.Collision.Width, 1);
-        Rectangle rightLine = new Rectangle(go.Collision.X + go.Collision.Width, go.Collision.Y, 1, go.Collision.Height);
-        Rectangle leftLine = new Rectangle(go.Collision.X, go.Collision.Y, 1, go.Collision.Height);
-        //Der tegnes en streg med tykkelsen 1 for hver side af Collision med collsionTexture med farven rød.
-        _spriteBatch.Draw(collisionTexture, topLine, Color.Red);
-        _spriteBatch.Draw(collisionTexture, bottomLine, Color.Red);
-        _spriteBatch.Draw(collisionTexture, rightLine, Color.Red);
-        _spriteBatch.Draw(collisionTexture, leftLine, Color.Red);
-    }
+    //public void DrawCollisionBox(GameObject go)
+    //{
+    //    //Der laves en streg med tykkelsen 1 for hver side af Collision.
+    //    Rectangle topLine = new Rectangle(go.Collision.X, go.Collision.Y, go.Collision.Width, 1);
+    //    Rectangle bottomLine = new Rectangle(go.Collision.X, go.Collision.Y + go.Collision.Height, go.Collision.Width, 1);
+    //    Rectangle rightLine = new Rectangle(go.Collision.X + go.Collision.Width, go.Collision.Y, 1, go.Collision.Height);
+    //    Rectangle leftLine = new Rectangle(go.Collision.X, go.Collision.Y, 1, go.Collision.Height);
+    //    //Der tegnes en streg med tykkelsen 1 for hver side af Collision med collsionTexture med farven rød.
+    //    _spriteBatch.Draw(collisionTexture, topLine, Color.Red);
+    //    _spriteBatch.Draw(collisionTexture, bottomLine, Color.Red);
+    //    _spriteBatch.Draw(collisionTexture, rightLine, Color.Red);
+    //    _spriteBatch.Draw(collisionTexture, leftLine, Color.Red);
+    //}
 
     protected override void Draw(GameTime gameTime)
     {
@@ -140,5 +140,4 @@ namespace TowerDefenceEksamensProjekt
         _spriteBatch.End();
         base.Draw(gameTime);
     }
-}
 }
